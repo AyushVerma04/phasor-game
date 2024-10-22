d67ef05ef6e5a3795edecc46d70b43ebaa6282f1
Anant Singh <anantsingh4444@gmail.com>
2024-10-22 23:13:20 +0530

    Add player moving animations, better map, and bug fixes

--- src/components/GameScene.js
+++ src/components/GameScene.js
@@ -24,74 +24,113 @@ const GameScene = () => {
     const game = new Phaser.Game(config);
 
     function preload() {
-      // Load player sprite
-      this.load.image('player', 'player.png');
       this.load.image('tiles', 'map.png')
+      this.load.image('map', 'mapCustom.png')
+      this.load.spritesheet('player', 'spriteMap.png',
+        { frameWidth: 32, frameHeight: 48 }
+      );
+      this.load.spritesheet('walk_down', 'walkdown.png',
+        { frameWidth: 40, frameHeight: 48 }
+      );
+      this.load.spritesheet('walk_up', 'walkup.png',
+        { frameWidth: 40, frameHeight: 48 }
+      );
+      this.load.spritesheet('walk_left', 'walkleft.png',
+        { frameWidth: 40, frameHeight: 48 }
+      );
+      this.load.spritesheet('walk_right', 'walkright.png',
+        { frameWidth: 40, frameHeight: 48 }
+      );
     }
 
     function create() {
-        // Add player
+      this.add.image(0, 0, 'map').setOrigin(0, 0).setScale(0.5);
+
+        this.anims.create({
+          key: 'walk_down',
+          frames: this.anims.generateFrameNumbers('walk_down', { start: 0, end: 3 }),
+          frameRate: 10,
+          repeat: -1
+        });
+
+        this.anims.create({
+          key: 'walk_left',
+          frames: this.anims.generateFrameNumbers('walk_left', { start: 0, end: 3 }),
+          frameRate: 10,
+          repeat: -1
+        });
+
+        this.anims.create({
+          key: 'walk_right',
+          frames: this.anims.generateFrameNumbers('walk_right', { start: 0, end: 3 }),
+          frameRate: 10,
+          repeat: -1
+        });
+
+        this.anims.create({
+          key: 'walk_up',
+          frames: this.anims.generateFrameNumbers('walk_up', { start: 0, end: 3 }),
+          frameRate: 10,
+          repeat: -1
+        });
+
+        this.anims.create({
+          key: 'stay',
+          frames: [ { key: 'walk_down', frame: 0 } ],
+          frameRate: 20
+        });
+
         this.player = this.physics.add.sprite(400, 300, 'player');
         this.player.setCollideWorldBounds(true);
-        this.player.setScale(2)
-        this.player.setDepth(1); // Player on top
-      
-        const map = this.make.tilemap({ tileWidth: 32, tileHeight: 32, width: 25, height: 19 });
-        const tiles = map.addTilesetImage('tiles');
-      
-        // Create the layers (ground and walls)
-        const groundLayer = map.createBlankLayer('Ground', tiles, 0, 0);
-        const wallLayer = map.createBlankLayer('Walls', tiles, 0, 0);
-      
-        // Fill the ground layer with a basic tile (index 0 for example)
-        groundLayer.fill(18);
-      
-        // Create walls by setting specific tiles on the wall layer
-        wallLayer.putTileAt(82, 10, 5);
-        wallLayer.putTileAt(82, 6, 5);
-        wallLayer.putTileAt(81, 7, 5);
-      
-        // Set layer depth
-        groundLayer.setDepth(0);
-        wallLayer.setDepth(0);
-      
-        // Add collision to wall tiles
-        wallLayer.setCollisionBetween(81, 82);
-      
-        // Create boundaries (walls)
-        const walls = this.physics.add.staticGroup();
-        walls.create(400, 0, null).setScale(2, 0.1).refreshBody();
-        walls.create(400, 600, null).setScale(2, 0.1).refreshBody();
-        walls.create(0, 300, null).setScale(0.1, 2).refreshBody();
-        walls.create(800, 300, null).setScale(0.1, 2).refreshBody();
-      
-        // Add collision between player and boundaries
-        this.physics.add.collider(this.player, wallLayer);
+        this.player.setBounce(0.2);
+
+        this.player.setScale(1.5)
       }
       
 
-    function update() {
-      const cursors = this.input.keyboard.createCursorKeys();
-
-      if (cursors.left.isDown) {
-        this.player.setVelocityX(-160);
-      } else if (cursors.right.isDown) {
-        this.player.setVelocityX(160);
-      } else {
-        this.player.setVelocityX(0);
+      function update() {
+        const cursors = this.input.keyboard.createCursorKeys();
+      
+        // Variable to track if any key is pressed
+        let moving = false;
+      
+        if (cursors.left.isDown) {
+          this.player.anims.play('walk_left', true);
+          this.player.setVelocityX(-160);
+          this.player.setVelocityY(0);
+          moving = true;
+        } else if (cursors.right.isDown) {
+          this.player.anims.play('walk_right', true);
+          this.player.setVelocityX(160);
+          this.player.setVelocityY(0);
+          moving = true;
+        } else {
+          this.player.setVelocityX(0);
+        }
+      
+        if (cursors.up.isDown) {
+          this.player.anims.play('walk_up', true);
+          this.player.setVelocityX(0);
+          this.player.setVelocityY(-160);
+          moving = true;
+        } else if (cursors.down.isDown) {
+          this.player.anims.play('walk_down', true);
+          this.player.setVelocityX(0);
+          this.player.setVelocityY(160);
+          moving = true;
+        } else {
+          this.player.setVelocityY(0);
+        }
+      
+        // If no movement keys are pressed, play the 'stay' animation
+        if (!moving) {
+          this.player.anims.play('stay', true);
+        }
       }
-
-      if (cursors.up.isDown) {
-        this.player.setVelocityY(-160);
-      } else if (cursors.down.isDown) {
-        this.player.setVelocityY(160);
-      } else {
-        this.player.setVelocityY(0);
-      }
-    }
+      
 
     return () => {
-      game.destroy(true); // Clean up when the component unmounts
+      game.destroy(true);
     };
   }, []);
 
